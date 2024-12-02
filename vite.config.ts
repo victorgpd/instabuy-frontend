5f48c45a56fcb4e476a2eafe2114771693a60519
victorgpd <victorguilherme2407@outlook.com>
2024-12-02 11:05:47 -0300

    versão 4.8

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/instabuy-frontend/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
